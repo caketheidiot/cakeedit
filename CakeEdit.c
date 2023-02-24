9af63056422b887488a90fff2ba8ec975e9736b2
Cake <126091307+caketheidiot@users.noreply.github.com>
2023-02-24 16:04:11 +0000

    Add filename length warning

--- CakeEdit.c
+++ CakeEdit.c
@@ -4,11 +4,32 @@
 #include <string.h>
 int main() {
 	char *str;
-	char *nm;
+	char *nm, c;
 	nm = (char *) malloc(12);
 	str = (char *) malloc(4096);
 	printf("NAME: \n");
 	scanf("%s", nm);
+	if (strlen(nm) > 12) {
+		printf("WARNING: FILENAME EXCEEDS 12 CHAR LIMIT \n");
+	}
+	FILE *f = fopen(nm, "w");
+        if (f == NULL)
+        {
+            printf("Error opening file! :( \n");
+            exit(1);
+        }
+	printf("EXISTING CONTENTS: ");
+
+	c = fgetc(f);
+    while (c != EOF)
+    {
+        c = fgetc(f);
+
+        printf("%c", c);
+        
+    }
+
+
 	printf("FILE CONTENTS: \n");
 	scanf("%s", str);
 	
@@ -16,17 +37,14 @@ int main() {
 	printf("\n");
 	
 	
-	FILE *f = fopen(nm, "w");
-	if (f == NULL)
-	{
-	    printf("Error opening file! :( \n");
-	    exit(1);
-	}
 	
+	
+        
+
 	/* print some text */
 	printf("Saving your stuff... \n");
 	fprintf(f, " %s\n", str);
-
+	free(f);	
 	free(str);
 	free(nm);
 	return 0;
