933307f1af4ecab0292f629c9eecb4b3cc9b86a3
caketheidiot <butex8066@tutanota.com>
2023-02-27 21:22:00 +0530

    Change variable names

--- main.c
+++ main.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 int main() {
-    char *str;
+    char *rbg;
     char *nm;
 
     nm = (char *) malloc(32);
-    str = (char *) malloc(8192);
+    rbg = (char *) malloc(8192);
     printf("Name: ");
     scanf("%s", nm);
     if (strlen(nm) > 12) {
         printf("ERROR: Filename exceeds 12 char limit");
         exit(1);
     }
-    if (strlen(str) > 4096) {
+    if (strlen(rbg) > 4096) {
         printf("ERROR: File exceeds 4096 char limit");
         exit(1);
     }
@@ -23,12 +23,10 @@ int main() {
             exit(1);
         }
     printf("Type your file: \n");
-    scanf("%s", str);
-
-    printf("Saving... \n");
-    fprintf(f, " %s\n", str);
+    scanf("%s", rbg);
+    ;fprintf(f, " %s\n", rbg);
     free(f);
-    free(str);
+    free(rbg);
     free(nm);
     return 0;
 }
