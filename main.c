abeb680ed079694415aa4208985ce1928109acc8
Cake <126091307+caketheidiot@users.noreply.github.com>
2023-03-11 08:56:04 +0000

    remove newlines

--- main.c
+++ main.c
@@ -6,18 +6,14 @@
 int main() {
     char rbg[BUF_SIZE];
     char *nm;
-    int ret;
     nm = (char *) malloc(32);
-
-
-    printf("\n");
+        
     printf("Name: ");
     scanf("%s", nm);
     if (strlen(nm) > 32) {
         printf("ERROR: Filename exceeds 32 char limit \n");
         exit(1);
     }
-
     FILE *f = fopen(nm, "w");
         if (f == NULL) {
             printf("Error opening file.");
@@ -25,22 +21,16 @@ int main() {
         }
     printf("Type your file: \n");
     getchar();                         // catching newline character.
-
-fgets(rbg, 50, stdin);
+    fgets(rbg, 8192, stdin);
     printf("\n");
-printf("%s", rbg);
-printf("\n");
-		
-    
-
+    printf("%s", rbg);
+    printf("\n");
     if (strlen(rbg) > 8192) {
         printf("ERROR: File exceeds 8192 char limit \n");
         exit(1);
     }
     fprintf(f, " %s\n", rbg);
-    
-    
     free(nm);
-
+    
     return 0;
 }
