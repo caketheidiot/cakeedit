a1a600cba8e3464f4b79840492a2bca04ce521a7
Cake <126091307+caketheidiot@users.noreply.github.com>
2023-03-11 15:14:39 +0000

    Update CakeEdit.c

--- CakeEdit.c
+++ CakeEdit.c
@@ -1,59 +1,40 @@
-/* This is CakeEdit v1.2.3 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main() {
-	char *str;
-	char *nm;
-	char c;
-	nm = (char *) malloc(12);
-	str = (char *) malloc(4096);
-	printf("NAME: \n");
-	scanf("%4s", nm);
-	if (strlen(nm) > 12) {
-		printf("ERROR: Filename exceeds 12 char limit \n");
-		exit(1);
-	}
-	if (strlen(str) > 4096) {
-		printf("ERROR: File contents exceeds 4096 char limit");
-		exit(1);
-	}
-	FILE *f = fopen(nm, "w");
-        if (f == NULL)
-        {
-            printf("Error opening file! :( \n");
+#define BUF_SIZE 8192
+int main(int argc, char *argv[]) {
+        if (argc != 3) {
+        printf("Usage: ./CakeEdit-* \r\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    char rbg[BUF_SIZE];
+    char *nm;
+    nm = (char *) malloc(32);
+                                
+    printf("Name: ");
+    scanf("%s", nm);
+    if (strlen(nm) > 32) {
+        printf("ERROR: Filename exceeds 32 char limit \r\n");
+        exit(1);
+    }
+    FILE *f = fopen(nm, "w");
+        if (f == NULL) {
+            printf("Error opening file.");
             exit(1);
         }
-	printf("EXISTING CONTENTS: ");
-
-	c = fgetc(f);
-    while (c != EOF)
-    {
-        c = fgetc(f);
-
-        printf("%c", c);
-        
-    }
-
-
-	printf("FILE CONTENTS: \n");
-	scanf("%s", str);
-	
-
-	printf("\n");
-	
-	
-	
-	
-        
-
-	/* print some text */
-	printf("Saving your stuff... \n");
-	fprintf(f, " %s\n", str);
-	free(f);
-	free(str);
-	free(nm);
-	free(c);
-	return 0;
+    printf("Type your file: \n");
+    getchar();                         // catching newline character.
+    fgets(rbg, 8192, stdin);
+    
+    if (strlen(rbg) > 8192) {
+        printf("ERROR: File exceeds 8192 char limit \r\n");
+        exit(1);
 }
+    fprintf(f, " %s\n", rbg);
+    free(nm);
+    free(f);
+    return 0;
+}
+        
 
