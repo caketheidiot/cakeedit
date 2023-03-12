78c81bdc7cb36b78762f8c19918fc0d185d5a3bc
Cake <126091307+caketheidiot@users.noreply.github.com>
2023-03-12 09:30:44 +0000

    allow changing of mode

--- main.c
+++ main.c
@@ -1,20 +1,24 @@
-#include <stdbool.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define BUF_SIZE 8192
+#define TYPE_MODE "a"
 int main() {
+    FILE *rcfptr = fopen("ck.conf", "r"); 
+    char *modef;
     char rbg[BUF_SIZE];
     char *nm;
+    modef = (char *) malloc(5);
+    fgets(modef, 5, rcfptr);
     nm = (char *) malloc(32);
         
     printf("Name: ");
     scanf("%s", nm);
     if (strlen(nm) > 32) {
-        printf("ERROR: Filename exceeds 32 char limit \n");
-        exit(1);
+	printf("ERROR: Filename exceeds limit");
     }
-    FILE *f = fopen(nm, "w");
+    FILE *f = fopen(nm, TYPE_MODE);
         if (f == NULL) {
             printf("Error opening file.");
             exit(1);
@@ -22,15 +26,14 @@ int main() {
     printf("Type your file: \n");
     getchar();                         // catching newline character.
     fgets(rbg, 8192, stdin);
-    printf("\n");
     
-    printf("\n");
     if (strlen(rbg) > 8192) {
         printf("ERROR: File exceeds 8192 char limit \n");
         exit(1);
     }
     fprintf(f, " %s\n", rbg);
     free(nm);
-    free(f); 
+    free(modef);
+     
     return 0;
 }
