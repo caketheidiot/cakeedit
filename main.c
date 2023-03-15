75b3ea33dd03bf214cead3c23488f8cf889cacd2
Cake <126091307+caketheidiot@users.noreply.github.com>
2023-03-15 15:57:53 +0000

    add

--- main.c
+++ main.c
@@ -1,38 +1,33 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define BUF_SIZE 8192
-#define TYPE_MODE "a"
+#define TYPE_MODE "a" // Defines mode to use (used in line 16) 
 int main() {
-    
-    char rbg[BUF_SIZE];
-    char *nm;
-    
-    
+    char *rbg; // Where text contents go
+    char *nm; // Filename
+    rbg = (char *) malloc(BUF_SIZE);
     nm = (char *) malloc(32);
-        
-    printf("Name: ");
+    puts("Name: ");
     scanf("%s", nm);
     if (strlen(nm) > 32) {
-	printf("ERROR: Filename exceeds limit");
+        puts("ERROR: Filename exceeds limit");
+	exit(1);
     }
     FILE *f = fopen(nm, TYPE_MODE);
         if (f == NULL) {
-            printf("Error opening file.");
+            puts("Error opening file.");
             exit(1);
         }
-    printf("Type your file: \n");
-    getchar();                         // catching newline character.
+    puts("Type your file: ");
+    getchar();    // catching newline character.
     fgets(rbg, 8192, stdin);
-    
     if (strlen(rbg) > 8192) {
         printf("ERROR: File exceeds 8192 char limit \n");
         exit(1);
     }
     fprintf(f, " %s\n", rbg);
     free(nm);
-    
-     
+    free(rbg);
     return 0;
 }
