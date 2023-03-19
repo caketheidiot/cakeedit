0f960004e355dd2e3fa4c8b97ba0fe2da9e114c3
Cake <126091307+caketheidiot@users.noreply.github.com>
2023-03-19 09:22:28 +0000

    split interface and the thing that does the writing

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define BUF_SIZE 8192
-#define TYPE_MODE "a" // Defines mode to use (used in line 16) 
+#include "f2.c" 
+ 
 int main() {
     char *rbg; // Where text contents go
     char *nm; // Filename
-    rbg = (char *) malloc(BUF_SIZE);
+    rbg = (char *) malloc(8192);
     nm = (char *) malloc(32);
     puts("Name: ");
     scanf("%s", nm);
@@ -14,11 +14,8 @@ int main() {
         puts("ERROR: Filename exceeds limit");
 	exit(1);
     }
-    FILE *f = fopen(nm, TYPE_MODE);
-        if (f == NULL) {
-            puts("Error opening file.");
-            exit(1);
-        }
+    
+        
     puts("Type your file: ");
     getchar();    // catching newline character.
     fgets(rbg, 8192, stdin);
@@ -26,8 +23,8 @@ int main() {
         printf("ERROR: File exceeds 8192 char limit \n");
         exit(1);
     }
-    fprintf(f, " %s\n", rbg);
-    free(nm);
+    write(rbg, nm);
     free(rbg);
+      
     return 0;
 }
